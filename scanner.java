a45c171454e021be3db435c9012f9ec8b68ee93d
Priyanka Jadhav <124450746+hp16coder@users.noreply.github.com>
2023-02-09 21:09:51 +0530

    Add files via upload

---
+++ scanner.java
@@ -0,0 +1,18 @@
+package placement_code;
+ import java.util.*;
+
+public class scanner {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	//input 
+		
+		Scanner sc = new Scanner(System.in);
+		 int a = sc.nextInt();
+		 int b = sc.nextInt();
+		 int c = a+b;
+		System.out.println(c);
+	}
+
+}
