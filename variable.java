3725ce0297162f5650efcdca30fe79540d8128c9
Priyanka Jadhav <124450746+hp16coder@users.noreply.github.com>
2023-02-09 20:14:53 +0530

    Add files via upload

---
+++ variable.java
@@ -0,0 +1,14 @@
+package placement_code;
+
+public class variable {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 16;
+		int b = 26;
+		int sum = a+b;
+		System.out.println(sum);
+
+	}
+
+}
